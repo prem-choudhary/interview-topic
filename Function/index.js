f7a47c28571a91038162430d6a5d6551c84c5eb5
Prem Choudhary <pk5480524@gmail.com>
2024-12-03 15:56:30 +0530

    function

---
+++ Function/index.js
@@ -0,0 +1,9 @@
+// let x = 10; 
+// functionouterFunction() { 
+//  return function innerFunction() { 
+//  console.log(x); 
+//  }; 
+// } 
+// const closure = outerFunction(); 
+// x = 20; closure();
+console.log(typeof NaN == typeof 2/'a')
